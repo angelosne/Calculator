3ea37f3152335aa01013adec1a7ff15fcafa57f3
angelosne <negkas.aggelos@gmail.com>
2020-03-14 21:37:57 +0200

    Create function

--- app/src/main/java/com/symmetron/calculator/MainActivity.java
+++ app/src/main/java/com/symmetron/calculator/MainActivity.java
@@ -1,171 +1,102 @@
 package com.symmetron.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.the_other);
 
-        Button button_0 = findViewById(R.id.button_0);
-        Button button_1 = findViewById(R.id.button_1);
-        Button button_2 = findViewById(R.id.button_2);
-        Button button_3 = findViewById(R.id.button_3);
-        Button button_4 = findViewById(R.id.button_4);
-        Button button_5 = findViewById(R.id.button_5);
-        Button button_6 = findViewById(R.id.button_6);
-        Button button_7 = findViewById(R.id.button_7);
-        Button button_8 = findViewById(R.id.button_8);
-        Button button_9 = findViewById(R.id.button_9);
-        Button button_plus = findViewById(R.id.button_plus);
-        Button button_minus = findViewById(R.id.button_minus);
-        Button button_division = findViewById(R.id.button_division);
-        Button button_multi = findViewById(R.id.button_multi);
-        Button button_equals = findViewById(R.id.button_equals);
+        Button button0 = findViewById(R.id.button_0);
+        Button button1 = findViewById(R.id.button_1);
+        Button button2 = findViewById(R.id.button_2);
+        Button button3 = findViewById(R.id.button_3);
+        Button button4 = findViewById(R.id.button_4);
+        Button button5 = findViewById(R.id.button_5);
+        Button button6 = findViewById(R.id.button_6);
+        Button button7 = findViewById(R.id.button_7);
+        Button button8 = findViewById(R.id.button_8);
+        Button button9 = findViewById(R.id.button_9);
+        Button buttonPlus = findViewById(R.id.button_plus);
+        Button buttonMinus = findViewById(R.id.button_minus);
+        Button buttonDivision = findViewById(R.id.button_division);
+        Button buttonMulti = findViewById(R.id.button_multi);
+        Button buttonEquals = findViewById(R.id.button_equals);
+
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        buttonPlus.setOnClickListener(this);
+        buttonMinus.setOnClickListener(this);
+        buttonMulti.setOnClickListener(this);
+        buttonEquals.setOnClickListener(this);
+        buttonDivision.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
         final EditText screen = findViewById(R.id.editText);
 
-        button_0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (screen.getText().toString().equals("0")){
-                    screen.setText("0");
-                }
-                else if (screen.length()<8)
-                    screen.append("0");
-            }
-        });
-        button_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (screen.getText().toString().equals("0")){
-                    screen.setText("1");
-                }
-                else if (screen.length()<8)
-                    screen.append("1");
-            }
-        });
-        button_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (screen.getText().toString().equals("0")){
-                    screen.setText("2");
-                }
-                else if (screen.length()<8)
-                    screen.append("2");
-            }
-        });
-        button_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (screen.getText().toString().equals("0")){
-                    screen.setText("3");
-                }
-                else if (screen.length()<8)
-                    screen.append("3");
-            }
-        });
-        button_4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (screen.getText().toString().equals("0")){
-                    screen.setText("4");
-                }
-                else if (screen.length()<8)
-                    screen.append("4");
-            }
-        });
-        button_5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (screen.getText().toString().equals("0")){
-                    screen.setText("5");
-                }
-                else if (screen.length()<8)
-                    screen.append("5");
-            }
-        });
-        button_6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (screen.getText().toString().equals("0")){
-                    screen.setText("6");
-                }
-                else if (screen.length()<8)
-                    screen.append("6");
-            }
-        });
-        button_7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (screen.getText().toString().equals("0")){
-                    screen.setText("7");
-                }
-                else if (screen.length()<8)
-                    screen.append("7");
-            }
-        });
-        button_8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (screen.getText().toString().equals("0")){
-                    screen.setText("8");
-                }
-                else if (screen.length()<8)
-                    screen.append("8");
-            }
-        });
-        button_9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (screen.getText().toString().equals("0")){
-                    screen.setText("9");
-                }
-                else if (screen.length()<8)
-                    screen.append("9");
-            }
-        });
-        button_plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                screen.setText("0");
-            }
-        });
-        button_minus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                screen.setText("0");
-            }
-        });
-        button_multi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                screen.setText("0");
-            }
-        });
-        button_division.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                screen.setText("0");
-            }
-        });
-        button_equals.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                screen.setText("0");
-            }
-        });
+        if (v.getId() == R.id.button_0) {
+            writeOnEditTextWhenButtonPressed("0");
+        } else if (v.getId() == R.id.button_1) {
+            writeOnEditTextWhenButtonPressed("1");
+        } else if (v.getId() == R.id.button_2) {
+            writeOnEditTextWhenButtonPressed("2");
+        } else if (v.getId() == R.id.button_3) {
+            writeOnEditTextWhenButtonPressed("3");
+        } else if (v.getId() == R.id.button_4) {
+            writeOnEditTextWhenButtonPressed("4");
+        } else if (v.getId() == R.id.button_5) {
+            writeOnEditTextWhenButtonPressed("5");
+        } else if (v.getId() == R.id.button_6) {
+            writeOnEditTextWhenButtonPressed("6");
+        } else if (v.getId() == R.id.button_7) {
+            writeOnEditTextWhenButtonPressed("7");
+        } else if (v.getId() == R.id.button_8) {
+            writeOnEditTextWhenButtonPressed("8");
+        } else if (v.getId() == R.id.button_9) {
+            writeOnEditTextWhenButtonPressed("9");
+        } else if (v.getId() == R.id.button_minus) {
+            screen.setText("0");
+        } else if (v.getId() == R.id.button_plus) {
+            screen.setText("0");
+        } else if (v.getId() == R.id.button_multi) {
+            screen.setText("0");
+        } else if (v.getId() == R.id.button_division) {
+            screen.setText("0");
+        } else if (v.getId() == R.id.button_equals) {
+            screen.setText("0");
+        }
+
+    }
 
 
+    private void writeOnEditTextWhenButtonPressed(String a) {
+        final EditText screen = findViewById(R.id.editText);
 
+        if (screen.getText().toString().equals("0")) {
+            screen.setText(a);
+        } else if (screen.length() < 8)
+            screen.append(a);
     }
 
 
